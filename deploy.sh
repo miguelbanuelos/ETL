4336a38a800d7548b551001fac4a922de3064599
Migue Bañuelos <kaiser.gdl@gmail.com>
2026-05-25 13:54:27 -0600

    fix: limpieza de nombre en deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd /docker/etl
 git config --global --add safe.directory /docker/etl
 git fetch --all
 git reset --hard origin/main
-docker compose up -d --build --force-recreate
+docker compose up -d --build --force-recreate --remove-orphans
